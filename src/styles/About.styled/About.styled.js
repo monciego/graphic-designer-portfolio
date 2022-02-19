b3dcb3d845324d86a74817cead19981af9c1f87c
Jericho <jerichobantiquete@gmail.com>
2022-02-19 14:47:46 +0800

    Responsive text

--- src/styles/About.styled/About.styled.js
+++ src/styles/About.styled/About.styled.js
@@ -14,7 +14,12 @@ export const StyledAbout = styled.section`
 export const AboutDetails = styled.div``;
 
 export const AboutText = styled.p`
+  font-size: 0.85rem;
   margin: 1rem 0;
   letter-spacing: 0.5px;
   color: var(--clr-light-secondary);
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    font-size: 1rem;
+  }
 `;
